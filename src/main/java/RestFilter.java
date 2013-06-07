5b5baa2bb9d61dcacd1d41591a35c3dbcd189754
oojdon <vsmysee@gmail.com>
2013-06-07 22:14:02 +0800

    create

---
+++ src/main/java/RestFilter.java
@@ -0,0 +1,25 @@
+import javax.servlet.*;
+import java.io.IOException;
+
+/**
+ * User: oojdon
+ * Date: 13-6-7
+ * Time: 下午10:02
+ */
+public class RestFilter implements Filter {
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
